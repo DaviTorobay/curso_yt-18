480163e319850c5ab4c37296d769c7aee0a99c28
DaviTorobay <dtorobay@findes.org.br>
2022-10-05 10:38:12 -0300

    Espaçamento botões

--- src/app/components/example/example.component.ts
+++ src/app/components/example/example.component.ts
@@ -24,24 +24,6 @@ export class ExampleComponent implements OnInit {
 
   currentTreinamento = undefined;
 
-  // treinamentos = [
-  //   {
-  //     id: 1,
-  //     name: 'COVID-19',
-  //     type: 'health',
-  //   },
-  //   {
-  //     id: 2,
-  //     name: 'NR-35',
-  //     type: 'security',
-  //   },
-  //   {
-  //     id: 3,
-  //     name: 'NR-10',
-  //     type: 'security',
-  //   },
-  // ];
-
   compareWith(o1: { id: any; }, o2: { some: (arg0: (o: any) => boolean) => any; id: any; }) {
     if (!o1 || !o2) {
       return o1 === o2;
